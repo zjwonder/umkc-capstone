32ec5a293e47036ddb509b57e7a93d0fe71b6e0e
rpwgc4 <rpwgc4@mail.umkc.edu>
2021-09-22 16:38:07 -0500

    Configure session and stylesheets

--- CommerceBankProject/Startup.cs
+++ CommerceBankProject/Startup.cs
@@ -26,6 +26,13 @@ namespace CommerceBankProject
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDistributedMemoryCache();
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromSeconds(10);
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+            });
             services.AddControllersWithViews();
             services.AddRazorPages();
             services.AddDbContext<CommerceBankDbContext>(options =>
@@ -55,6 +62,8 @@ namespace CommerceBankProject
 
             app.UseAuthentication();
             app.UseAuthorization();
+
+            app.UseSession();
 
             app.UseEndpoints(endpoints =>
             {
