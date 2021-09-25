37c82223cb4e17fc0d5d9e36adc9345816501dc7
Colin McNish <newvader99@gmail.com>
2021-09-24 19:53:29 -0500

    added text box for paging. Currently fixed page size of 10

--- CommerceBankProject/Models/TIndexViewModel.cs
+++ CommerceBankProject/Models/TIndexViewModel.cs
@@ -14,14 +14,19 @@ namespace CommerceBankProject.Models
         public DateTime toDate { get; set; }
         public string descSearch { get; set; }
         public string debug { get; set; }
+        public int pageNumber { get; set; }
 
-        public TIndexViewModel(List<Transaction> transactions, List<AccountRecord> accounts, DateTime start, DateTime end, string desc="")
+        public int pageSize { get; set; } = 10;
+
+        public TIndexViewModel(List<Transaction> transactions, List<AccountRecord> accounts, DateTime start, DateTime end, string desc="", int PageNumber = 1)
         {
             tList = transactions;
             actList = accounts;
             descSearch = desc;
             fromDate = start;
             toDate = end;
+            pageNumber = PageNumber;
+
         }
     }
 }
