cd1bad301e0ce9e5d4713d90e3cc93300bab5752
rpwgc4 <rpwgc4@mail.umkc.edu>
2021-10-11 12:22:14 -0500

    Style inputs

--- CommerceBankProject/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ CommerceBankProject/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -4,9 +4,11 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using CommerceBankProject.Areas.Identity.Data;
+using CommerceBankProject.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace CommerceBankProject.Areas.Identity.Pages.Account.Manage
 {
@@ -14,13 +16,16 @@ namespace CommerceBankProject.Areas.Identity.Pages.Account.Manage
     {
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly CommerceBankDbContext _context;
 
         public IndexModel(
             UserManager<ApplicationUser> userManager,
-            SignInManager<ApplicationUser> signInManager)
+            SignInManager<ApplicationUser> signInManager,
+            CommerceBankDbContext context)
         {
             _userManager = userManager;
             _signInManager = signInManager;
+            _context = context;
         }
 
         public string Username { get; set; }
@@ -36,18 +41,27 @@ namespace CommerceBankProject.Areas.Identity.Pages.Account.Manage
             [Phone]
             [Display(Name = "Phone number")]
             public string PhoneNumber { get; set; }
+            [Display(Name = "First Name")]
+            public string firstName { get; set; }
+            [Display(Name = "Last Name")]
+            public string lastName { get; set; }
         }
 
         private async Task LoadAsync(ApplicationUser user)
         {
-            var userName = await _userManager.GetUserNameAsync(user);
-            var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
+            var u = await _userManager.FindByIdAsync(user.Id);
+            var userName = u.UserName;
+            var phoneNumber = u.PhoneNumber;
+            var first = u.firstName;
+            var last = u.lastName;
 
             Username = userName;
 
             Input = new InputModel
             {
-                PhoneNumber = phoneNumber
+                PhoneNumber = phoneNumber,
+                firstName = first,
+                lastName = last
             };
         }
 
@@ -77,13 +91,27 @@ namespace CommerceBankProject.Areas.Identity.Pages.Account.Manage
                 return Page();
             }
 
-            var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
+            var u = await _userManager.FindByIdAsync(user.Id);
+            var phoneNumber = u.PhoneNumber;
             if (Input.PhoneNumber != phoneNumber)
             {
                 var setPhoneResult = await _userManager.SetPhoneNumberAsync(user, Input.PhoneNumber);
                 if (!setPhoneResult.Succeeded)
                 {
                     StatusMessage = "Unexpected error when trying to set phone number.";
+                    return RedirectToPage();
+                }
+            }
+
+            var first = u.firstName;
+            var last = u.lastName;
+            if (Input.firstName != first || Input.lastName != last)
+            {
+                string query = "Update [AspNetUsers] set firstName = {0}, lastName = {1} where Id = {2}";
+                var result = await _context.Database.ExecuteSqlRawAsync(query, Input.firstName, Input.lastName, u.Id);
+                if (result == 0)
+                {
+                    StatusMessage = "Unexpected error when trying to set customer name.";
                     return RedirectToPage();
                 }
             }
