05152ce8c553c33b9e8c85fc2ff7549f6c4ddfc8
rpwgc4 <rpwgc4@mail.umkc.edu>
2021-11-18 12:24:09 -0600

    Site-wide styling

--- CommerceBankProject/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ CommerceBankProject/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -96,10 +96,6 @@ namespace CommerceBankProject.Areas.Identity.Pages.Account
                     _logger.LogInformation("User logged in.");
                     return LocalRedirect(returnUrl);
                 }
-                if (result.RequiresTwoFactor)
-                {
-                    return RedirectToPage("./LoginWith2fa", new { ReturnUrl = returnUrl, RememberMe = Input.RememberMe });
-                }
                 if (result.IsLockedOut)
                 {
                     _logger.LogWarning("User account locked out.");
