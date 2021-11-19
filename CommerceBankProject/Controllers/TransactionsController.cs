81da83d91c48dd73f691e648b0a2d6bfca37ec01
Zach Underwood <zju.0228@gmail.com>
2021-11-18 18:55:06 -0600

    add .xlsx download feature, format html

--- CommerceBankProject/Controllers/TransactionsController.cs
+++ CommerceBankProject/Controllers/TransactionsController.cs
@@ -1,25 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
+using System.Data;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
 using CommerceBankProject.Data;
 using CommerceBankProject.Models;
 using System.Security.Claims;
+using ClosedXML.Excel;
 
 namespace CommerceBankProject.Controllers
 {
     public class TransactionsController : Controller
     {
+
         private readonly CommerceBankDbContext _context;
 
         public TransactionsController(CommerceBankDbContext context)
         {
             _context = context;
         }
+
 
         // GET: Transactions
         [Authorize]
@@ -35,18 +39,21 @@ namespace CommerceBankProject.Controllers
             string dateQuery = "Select top 1 onDate from [Transaction] where customerID = {0} order by onDate";
             DateRecord record = await _context.Date.FromSqlRaw(dateQuery, user.customerID).FirstOrDefaultAsync();
             DateTime fromDate = record.onDate;
-            record = await _context.Date.FromSqlRaw(dateQuery+" desc", user.customerID).FirstOrDefaultAsync();
+            record = await _context.Date.FromSqlRaw(dateQuery + " desc", user.customerID).FirstOrDefaultAsync();
             DateTime toDate = record.onDate;
             TIndexViewModel vmod = new TIndexViewModel(tList, actList, fromDate, toDate);
-            
+
             return View(vmod);
         }
+
+
         [Authorize]
-        public async Task<IActionResult> Dashboard()
+        public IActionResult Dashboard()
         {
             return View();
         }
-        
+
+
         [Authorize]
         public async Task<IActionResult> FilterIndex(string actFilter, string descFilter, string fromDate, string toDate, string pageNumber)
         {
@@ -77,10 +84,11 @@ namespace CommerceBankProject.Controllers
             splitDate = fromDate.Split('-');
             DateTime fDate = new DateTime(int.Parse(splitDate[0]), int.Parse(splitDate[1]), int.Parse(splitDate[2]));
             List<AccountRecord> actList = await _context.Account.FromSqlRaw(actQuery, user.customerID).ToListAsync();
-            TIndexViewModel vmod = new TIndexViewModel(tList, actList, fDate, tDate, descFilter, int.Parse(pageNumber),actFilter);
-            
+            TIndexViewModel vmod = new TIndexViewModel(tList, actList, fDate, tDate, descFilter, int.Parse(pageNumber), actFilter);
+
             return View("Index", vmod);
         }
+
 
         // GET: Transactions/Details/
         [Authorize]
@@ -101,19 +109,16 @@ namespace CommerceBankProject.Controllers
             return View(transaction);
         }
 
+
         // GET: Transactions/Create
-        
         [Authorize]
         public IActionResult Create()
         {
             return View();
         }
-        
 
-
-        
         [Authorize]
-        public async Task<IActionResult> CreateSetup ()
+        public async Task<IActionResult> CreateSetup()
         {
             var claim = User.FindFirst(ClaimTypes.NameIdentifier);
             string userID = claim.Value;
@@ -127,7 +132,7 @@ namespace CommerceBankProject.Controllers
             return View("Create", t);
 
         }
-        
+
 
         // POST: Transactions/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
@@ -135,53 +140,52 @@ namespace CommerceBankProject.Controllers
         [Authorize]
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(string tranActFilter,string transType,decimal amount,string description,string category)
+        public async Task<IActionResult> Create(string tranActFilter, string transType, decimal amount, string description, string category)
         {
-            
-            
-                var claim = User.FindFirst(ClaimTypes.NameIdentifier);
-                string userID = claim.Value;
-                var user = await _context.Users.Where(u => u.Id == userID).FirstOrDefaultAsync();
-                Transaction t = new Transaction();
-
-                t.customerID = user.customerID;
-                t.actID = tranActFilter;
-                t.transType = transType;
-                t.amount = amount;
-                t.description = description;
-                t.userEntered = true;
-                t.category = category;
-                t.onDate = DateTime.Now;
 
 
-                string actBalance = "Select top 1 * from [Transaction] where customerID = {0} and actID = {1} order by onDate desc";
+            var claim = User.FindFirst(ClaimTypes.NameIdentifier);
+            string userID = claim.Value;
+            var user = await _context.Users.Where(u => u.Id == userID).FirstOrDefaultAsync();
+            Transaction t = new Transaction();
 
-                Transaction prevTopTransaction = await _context.Transaction.FromSqlRaw(actBalance, user.customerID,t.actID).FirstOrDefaultAsync();
-
-                decimal userBalance = prevTopTransaction.balance;
-
-                t.actType = prevTopTransaction.actType;
-
-                
-                if (t.transType == "DR")
-                {
-                    t.balance = userBalance - t.amount;
-                }
-                else
-                {
-                    t.balance = userBalance + t.amount;
-
-                }
+            t.customerID = user.customerID;
+            t.actID = tranActFilter;
+            t.transType = transType;
+            t.amount = amount;
+            t.description = description;
+            t.userEntered = true;
+            t.category = category;
+            t.onDate = DateTime.Now;
 
 
-                _context.Add(t);
-                await _context.SaveChangesAsync();
-                //NotificationsController temp = new NotificationsController(_context);
-                //temp.GenerateOnInsertion(t);
-                return RedirectToAction(nameof(Index));
+            string actBalance = "Select top 1 * from [Transaction] where customerID = {0} and actID = {1} order by onDate desc";
+
+            Transaction prevTopTransaction = await _context.Transaction.FromSqlRaw(actBalance, user.customerID, t.actID).FirstOrDefaultAsync();
+
+            decimal userBalance = prevTopTransaction.balance;
+
+            t.actType = prevTopTransaction.actType;
+
+
+            if (t.transType == "DR")
+            {
+                t.balance = userBalance - t.amount;
             }
-            
-        
+            else
+            {
+                t.balance = userBalance + t.amount;
+
+            }
+
+
+            _context.Add(t);
+            await _context.SaveChangesAsync();
+            //NotificationsController temp = new NotificationsController(_context);
+            //temp.GenerateOnInsertion(t);
+            return RedirectToAction(nameof(Index));
+        }
+
 
         // GET: Transactions/Delete/5
         [Authorize]
@@ -212,6 +216,54 @@ namespace CommerceBankProject.Controllers
             _context.Transaction.Remove(transaction);
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
+        }
+
+        [Authorize]
+        [HttpPost, ActionName("Export")]
+        public async Task<IActionResult> Export()
+        {
+            var claim = User.FindFirst(ClaimTypes.NameIdentifier);
+            string userID = claim.Value;
+            var user = await _context.Users.Where(u => u.Id == userID).FirstOrDefaultAsync();
+
+            var transactions = await _context.Transaction.Where(t => t.customerID == user.customerID).ToListAsync();
+
+            DataTable dt = new DataTable("Transactions");
+            dt.Columns.AddRange(new DataColumn[8] 
+            { 
+                new DataColumn("Account ID"),
+                new DataColumn("Account Type"),
+                new DataColumn("Date"),
+                new DataColumn("Description"),
+                new DataColumn("Category"),
+                new DataColumn("Transaction Type"),
+                new DataColumn("Amount"),
+                new DataColumn("Balance"),
+            });
+
+            foreach (var t in transactions) 
+            {
+                dt.Rows.Add(
+                    t.actID,
+                    t.actType,
+                    t.onDate,
+                    t.description,
+                    t.category,
+                    t.transType,
+                    t.amount,
+                    t.balance
+                    );
+            }
+
+            using (XLWorkbook wb = new XLWorkbook())
+            {
+                wb.Worksheets.Add(dt);
+                using (MemoryStream stream = new MemoryStream())
+                {
+                    wb.SaveAs(stream);
+                    return File(stream.ToArray(), "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "Transactions.xlsx");
+                }
+            }
         }
 
         private bool TransactionExists(int id)
